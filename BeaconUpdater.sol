6c76f079ea999336694fd7e5276a6b68846c56ca
Aditya Kumar Pandey <ap7596112@gmail.com>
2023-05-23 22:45:21 +0530

    Update BeaconUpdater.sol

--- BeaconUpdater.sol
+++ BeaconUpdater.sol
@@ -1,36 +1,20 @@
-pragma solidity ^0.6.0 ;
+pragma solidity ^0.6.0;
 
 contract BeaconUpdater {
     address public beaconContract;
     address public newBeaconAddress;
-
-    bool public beaconUpdateProposed;
-    bool public beaconUpdateApproved;
 
     constructor(address _beaconContract, address _newBeaconAddress) public {
         beaconContract = _beaconContract;
         newBeaconAddress = _newBeaconAddress;
     }
 
-    function proposeBeaconUpdate() external {
-        require(!beaconUpdateProposed, "Beacon update already proposed");
-        Beacon(beaconContract).proposeBeacon(address(this));
-        beaconUpdateProposed = true;
-    }
+    function updateBeacon() external {
+        bytes4 selector = bytes4(keccak256("approveBeacon(address,address)"));
+        bytes memory data = abi.encodeWithSelector(selector, address(this), newBeaconAddress);
 
-    function approveBeaconUpdate() external {
-        require(beaconUpdateProposed, "Beacon update not yet proposed");
-        Beacon(beaconContract).approveBeacon(address(this), newBeaconAddress);
-        beaconUpdateApproved = true;
+        // Use delegate call to update the beacon address
+        (bool success, ) = beaconContract.delegatecall(data);
+        require(success, "Beacon update failed");
     }
-
-    function executeBeaconUpdate() external {
-        require(beaconUpdateApproved, "Beacon update not yet approved");
-        Beacon(beaconContract).approveBeacon(address(this), newBeaconAddress);
-    }
-}
-
-interface Beacon {
-    function proposeBeacon(address _newAdmin) external;
-    function approveBeacon(address _expectedAdmin, address _newBeacon) external;
 }
